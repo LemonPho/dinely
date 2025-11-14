f3d363624dc26af973bca88f6e1a51d7d25e45c6
BenitoCamelo2 <louismatthew1112@gmail.com>
2025-11-13 19:33:47 -0600

    inicio

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Reserva from "./components/reservation.jsx";
-import Menu from "./components/menu.jsx";        // Página de Menú
-import Ubicacion from "./components/location.jsx"; // Nueva página Ubicación
-import MisReservas from "./components/reservations.jsx";
-import Login from "./components/login.jsx";
-import Layout from "./components/layout.jsx";
-import MainPage from "./components/main-page.jsx";
+import Reserva from "./components-user/reservation.jsx";
+import Menu from "./components-user/menu.js";        // Página de Menú
+import Ubicacion from "./components-user/location.js"; // Nueva página Ubicación
+import MisReservas from "./components-user/reservations.jsx";
+import Login from "./components-user/login.jsx";
+import Layout from "./components-user/layout.js";
+import MainPage from "./components-user/main-page.jsx";
+import AdminDashboard from "./components-admin/admin-dashboard.jsx";
 
 import "./styles/global.css";
 import { ProveedorMensajesContexto } from "./application-context/messages-context.jsx";
@@ -22,13 +23,16 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <UserContextProvider>
           <BrowserRouter>
             <Routes>
-              <Route element={<Layout />}>
+              <Route path="" element={<Layout />}>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/reserva" element={<Reserva />} />
                 <Route path="/menu" element={<Menu />} />
                 <Route path="/ubicacion" element={<Ubicacion />} />
                 <Route path="/mis-reservas" element={<MisReservas />} />
                 <Route path="/login" element={<Login />} />
+              </Route>
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route path="/admin" element={<AdminDashboard />} />
               </Route>
             </Routes>
           </BrowserRouter>
